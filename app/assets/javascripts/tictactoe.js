06ae5e713a0607a280a9442313b2e9888f5857ff
aburk3 <aburk3@gmail.com>
2019-04-12 06:55:03 -0500

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -29,16 +29,47 @@ function attachEventListeners() {
   });
 }
 
+// Update the square with the token of the current player 'X' or 'O'
 function updateState(square) {
   var token = player();
   $(square).text(token);
 }
 
+// Executes a turn
 function doTurn(square) {
   updateState(square);
   turn++;
+  if (checkWinner()) {
+    resetBoard();
+    saveGame();
+  }
 }
 
+// Check state of board, compare board to various WINNING_COMBOS
+function checkWinner() {
+  let board = {};
+  let winner = false;
+
+  $('td').text((index, square) => board[index] = square);
+
+  WINNING_COMBOS.some(function(combo) {
+    if (board[combo[0]] !== "" && board[combo[0]] === board[combo[1]] && board[combo[1]] === board[combo[2]]) {
+      setMessage(`Player ${board[combo[0]]} Won!`);
+      return winner = true;
+    }
+  });
+  return winner;
+}
+
+// Sets a message for if a Winner or Tie occurs
 function setMessage(string) {
     $('#message').text(string);
+}
+
+function saveGame() {
+
+}
+
+function resetBoard() {
+  $('td').empty();
 }
